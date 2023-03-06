8db9a32e7648d3b2d40e58d1fb4528614793026d
Mikail Simsek <mikailsimsek.trb@gmail.com>
2023-03-06 16:44:29 +0100

    Create dummy data and render it

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 
+posts = [
+{    'author': 'Mika Sims',
+    'title': 'Blog Post 1',
+    'content': 'First post content',
+    'created_at': 'May 27, 2022'},
 
+{    'author': 'Jane Doe',
+    'title': 'Blog Post 2',
+    'content': 'Second post content',
+    'created_at': 'March 14, 2022'}
+]
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context ={
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
