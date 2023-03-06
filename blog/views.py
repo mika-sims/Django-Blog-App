d642bf5c01f2363f537d218a3151374fbc6ffcba
Mikail Simsek <mikailsimsek.trb@gmail.com>
2023-03-06 18:43:51 +0100

    Delete duumy data and import Post model

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-{    'author': 'Mika Sims',
-    'title': 'Blog Post 1',
-    'content': 'First post content',
-    'created_at': 'May 27, 2022'},
-
-{    'author': 'Jane Doe',
-    'title': 'Blog Post 2',
-    'content': 'Second post content',
-    'created_at': 'March 14, 2022'}
-]
+from .models import Post
 
 def home(request):
     context ={
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
